2f4f4cae2c6786c4c002c710d73480ea23ee50cc
Svitlana Korneeva <kso16101@gmail.com>
2021-03-25 19:52:42 +0200

    addcontact

--- src/App.js
+++ src/App.js
@@ -1,81 +1,36 @@
 import React, { Component } from "react";
 import "./App.css";
-import Statistics from "./components/Statistics/Statistics";
-import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
-import Section from "./components/Section/Section";
-import Notification from "./components/Notification/Notification";
+import PhoneBook from "./components/PhoneBook/PhoneBook"
+
 
 
 class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+state = {
+  contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+  name: '',
+    number: '',
+}
+  addContact = text => {
+    const con = this.state.contacts
+    
+    console.log(text)
+    this.setState(prevState => ({
+      contacts: [con, ... prevState.contacts]
+    }))
+  }
   
-  // handleGood = () => {
-  //   this.setState(prevState => ({
-  //     good: prevState.good +1,
-
-  //   }))
-  // };
-  // handleNeutral = () => {
-  //   this.setState(prevState => ({
-  //     neutral: prevState.neutral +1,
-
-  //   }))
-  // };
+ 
+     
+    render() {
   
-  // handleBad = () => {
-  //   this.setState(prevState => ({
-  //     bad: prevState.bad +1,
+      return (
 
-  //   }))
-  // };
-  onLeaveFeedback = (e) => {
-    const name = e.target.name;
-    this.setState((prevState) => ({
-      [name]: prevState[name] + 1
-    }));
-  };
-  
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-  
-  countPositiveFeedbackPercentage = () => {
-    const result = this.countTotalFeedback();
-    const { good } = this.state;
-    const percentage = (good * 100) / result;
-    return Math.round(percentage);
-  };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-  
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={this.onLeaveFeedback} />
-        </Section>
-
-        <Section title="Statistics" >
-
-          {total === 0 ? (<Notification message="No feedback given" />) : 
-
-          (<Statistics
-      
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-              positivePercentage={positivePercentage} />
-    )}
-          
-        </Section>
-      </div>
+        <PhoneBook onAddContact={this.addContact}  />
+     
     );
   }
 }

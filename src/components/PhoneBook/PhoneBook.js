b5a1099c46aa09cc4d504c53fd4875510a6c95fc
Svitlana Korneeva <kso16101@gmail.com>
2021-03-25 22:04:43 +0200

    add contact finish

--- src/components/PhoneBook/PhoneBook.js
+++ src/components/PhoneBook/PhoneBook.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import s from "./PhoneBook.module.css";
-import { v4 as id } from "uuid";
+import { v4 as genId } from "uuid";
 
 class PhoneBook extends Component {
   state = {
-    contacts: [],
+    contacts: [ {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
     name: '',
     number: '',
   }
@@ -18,10 +22,17 @@ class PhoneBook extends Component {
     e.preventDefault();
     console.log(this.state)
 
-    const {name, number} = this.state;
-    this.props.onAddContact({name, number})
+    const { name, number } = this.state;
+    const newContact = {genId, name, number}
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts]
+    }))
+    
+    // this.props.onAddContact({name, number})
     this.setState({ name: '', number: '' });
     // this.props.onAddContact(this.state.number)
+    
+   
    
 
     // this.props.onAddContact({ ...this.state });
